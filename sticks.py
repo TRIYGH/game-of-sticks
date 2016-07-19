0f057d6ee545e8ff789e8821f6ae4462db1333e1
T. Robert Ward <trironyard@icloud.com>
2016-07-19 13:07:27 -0400

    basic working version

--- sticks.py
+++ sticks.py
@@ -9,7 +9,7 @@ def get_game_sticks():
 
 def player1_choice():
     while True:
-        pick = input("\nHow many sticks would you like to pick up? ")
+        pick = input("\nPLAYER 1:  How many sticks would you like to pick up? ")
         if validate_input(pick):
             if int(pick) in [1,2,3]:
                 return int(pick)
@@ -17,7 +17,7 @@ def player1_choice():
 
 def player2_choice():
     while True:
-        pick = input("\nHow many sticks would you like to pick up? ")
+        pick = input("\nPLAYER 2:  How many sticks would you like to pick up? ")
         if validate_input(pick):
             if int(pick) in [1,2,3]:
                 return int(pick)
@@ -35,22 +35,36 @@ def validate_input(data_in):
 
 
 def main():
+    P1 = "won"
+    P2 = "won"
+    print('\n\n')
+    print('*'*30)
+    print('***\tPick-Up-Sticks     ***')
+    print('*'*30)
+    print('\n\n')
+
+    total_sticks = get_game_sticks()
+
     while True:
-        print('\n\n')
-        print('*'*30)
-        print('***\tPick-Up-Sticks     ***')
-        print('*'*30)
-        print('\n\n')
-
-        total_sticks = get_game_sticks()
-
         update_game(total_sticks)
 
         sticks_picked = player1_choice()
         total_sticks -= sticks_picked
+
         update_game(total_sticks)
+
+        if total_sticks <= 0:
+            P1 = "lost"
+            break
+
         sticks_picked = player2_choice()
         total_sticks -= sticks_picked
+
+        if total_sticks <= 0:
+            P2 = "lost"
+            break
+
+    print("\n\nPlayer 1 {}\tPlayer 2 {}\n\n".format(P1,P2))
 
 
 if __name__ == "__main__":
--- sticks_test.py
+++ sticks_test.py
@@ -5,3 +5,6 @@ from sticks import validate_input
 def test_input():
     assert validate_input("3") == True
     assert validate_input("d") == False
+
+
+def tests_ss
